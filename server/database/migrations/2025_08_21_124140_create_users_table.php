4736b3609c1a1eef9db7f8883a17f7d1ea355f33
Hussein-alayan <alayen.hussein@gmail.com>
2025-08-21 17:03:45 +0300

    added email_verified_at column and correcting timestamps method in user table

--- server/database/migrations/2025_08_21_124140_create_users_table.php
+++ server/database/migrations/2025_08_21_124140_create_users_table.php
@@ -12,10 +12,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone')->nullable();
             $table->boolean('is_active')->default(true);
-            $table->timestampsTz();
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
