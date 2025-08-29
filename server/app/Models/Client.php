45865310ca5f8ec54051a1df6871224e94baf8c5
Hussein-alayan <alayen.hussein@gmail.com>
2025-08-29 15:03:53 +0300

    feat(model): define Client model with fillable attributes and relationships to Business and Booking

--- server/app/Models/Client.php
+++ server/app/Models/Client.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+    protected $fillable = [
+        'business_id',
+        'name',
+        'phone',
+        'email',
+        'no_show_count',
+    ];
+
+    public function business()
+    {
+        return $this->belongsTo(Business::class);
+    }
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
