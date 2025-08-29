25797feee0612b467f51ab796c763e5f6d23c156
Hussein-alayan <alayen.hussein@gmail.com>
2025-08-29 15:01:44 +0300

    feat(model): define BookingRule model with fillable attributes and relationship to Business

--- server/app/Models/BookingRule.php
+++ server/app/Models/BookingRule.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class BookingRule extends Model
 {
-    //
+    protected $fillable = [
+        'business_id',
+        'hours_of_operation',
+        'buffer_time_minutes',
+        'cancellation_policy',
+    ];
+
+    protected $casts = [
+        'hours_of_operation' => 'array',
+    ];
+
+    public function business()
+    {
+        return $this->belongsTo(Business::class);
+    }
 }
