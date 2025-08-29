c46f2b3146a63f2e20565257fa154fe9c81cc055
Hussein-alayan <alayen.hussein@gmail.com>
2025-08-29 19:32:16 +0300

    refactor(auth): move LoginRequest and RegisterRequest to AuthRequests namespace

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Controllers;
-use App\Http\Requests\RegisterRequest;
-use App\Http\Requests\LoginRequest;
+use App\Http\Requests\AuthRequests\RegisterRequest;
+use App\Http\Requests\AuthRequests\LoginRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
