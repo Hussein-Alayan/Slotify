5f83e025c73af5e0c01a145dc5274212b1eb9c1d
Hussein-alayan <alayen.hussein@gmail.com>
2025-08-29 14:34:39 +0300

    feat(migration): create communication_channels table with necessary fields and constraints

---
+++ server/database/migrations/2025_08_29_112432_create_communication_channels_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('communication_channels', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('business_id')->constrained('businesses')->onDelete('cascade');
+            $table->enum('channel_type', ['whatsapp', 'sms', 'email']);
+            $table->string('api_key')->nullable();
+            $table->string('token')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('email')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('communication_channels');
+    }
+};
