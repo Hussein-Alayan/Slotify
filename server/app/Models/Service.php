7413a62e7c6522eabb26ac2da6af7dc76c924eb0
Hussein-alayan <alayen.hussein@gmail.com>
2025-08-29 15:00:05 +0300

    feat(model): implement Service model with fillable attributes and relationships to Business and Booking

--- server/app/Models/Service.php
+++ server/app/Models/Service.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    //
+    protected $fillable = [
+        'business_id',
+        'name',
+        'duration_minutes',
+        'price',
+        'description',
+    ];
+
+    public function business()
+    {
+        return $this->belongsTo(Business::class);
+    }
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
