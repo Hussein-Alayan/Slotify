f5474a7dfbb0f532cd3df7f9b7760823d4993d58
Hussein-alayan <alayen.hussein@gmail.com>
2025-08-29 14:34:34 +0300

    feat(migration): create booking_rules table with necessary fields and constraints

---
+++ server/database/migrations/2025_08_29_112428_create_booking_rules_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('booking_rules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('business_id')->constrained('businesses')->onDelete('cascade');
+            $table->json('hours_of_operation');
+            $table->integer('buffer_time_minutes')->nullable();
+            $table->string('cancellation_policy')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('booking_rules');
+    }
+};
