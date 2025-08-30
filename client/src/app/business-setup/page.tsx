73a03d65b54843afb8ec9fa166875face868016b
Hussein-alayan <alayen.hussein@gmail.com>
2025-08-30 13:21:57 +0300

    feat(page): create BusinessSetupPage component for business profile setup

---
+++ client/src/app/business-setup/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BusinessSetupPage = () => {
+  return (
+    <div>
+      <h1>Business Setup</h1>
+      {/* Business Profile form and setup steps will go here */}
+    </div>
+  );
+};
+
+export default BusinessSetupPage;
