d47251742de45a8804affbe06cb9a80aa6f6f1cd
Hussein-alayan <alayen.hussein@gmail.com>
2025-08-31 22:36:11 +0300

    feat: add SetupSidebar component for improved navigation in Business Setup

--- client/src/app/Business-setup/containers/business-setup.tsx
+++ client/src/app/Business-setup/containers/business-setup.tsx
@@ -15,6 +15,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { SetupSidebar } from "../Components/SetupSidebar";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Switch } from "@/components/ui/switch";
               <Card>
