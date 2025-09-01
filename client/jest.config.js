3d8c0498f8b6abf190e59c13f5a9c7fa14658bf0
Hussein-alayan <alayen.hussein@gmail.com>
2025-09-01 21:27:10 +0300

    fix: remove Babel configuration and update Jest config to use ts-jest preset

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,11 +1,9 @@
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   moduleNameMapper: {
     // Support for @ alias
     '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
   },
 };
