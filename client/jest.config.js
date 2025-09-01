e5b5b15b9613d75bf63f8085c2dac650f82ce804
Hussein-alayan <alayen.hussein@gmail.com>
2025-09-01 16:45:12 +0300

    Add Jest configuration and setup for testing
    - Created jest.config.js to configure Jest with TypeScript and jsdom environment.
    - Added module name mapping for alias support and ignored specific test paths.
    - Created setupTests.ts to import custom matchers from @testing-library/jest-dom.

---
+++ client/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  moduleNameMapper: {
+    // Support for @ alias
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+};
