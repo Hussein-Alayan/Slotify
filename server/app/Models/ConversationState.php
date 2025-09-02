b07bdde8f86d38b9b41cff4e4efae28b14285c40
Hussein-alayan <alayen.hussein@gmail.com>
2025-09-02 17:23:51 +0300

    feat(models): add fillable attributes and casts to ConversationState model

--- server/app/Models/ConversationState.php
+++ server/app/Models/ConversationState.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class ConversationState extends Model
 {
-    //
+    protected $fillable = ['client_id', 'business_id', 'state'];
+
+    protected $casts = [
+        'state' => 'array',
+    ];
+
+    public $timestamps = false; // since we use last_updated instead
 }
+
