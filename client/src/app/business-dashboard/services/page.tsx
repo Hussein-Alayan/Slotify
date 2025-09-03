2e465a55aa1885ce82ed12f4f48b8dd49a408a99
Hussein-alayan <alayen.hussein@gmail.com>
2025-09-03 23:04:45 +0300

    refactor: simplify ServicesPage component by removing Sidebar

--- client/src/app/business-dashboard/services/page.tsx
+++ client/src/app/business-dashboard/services/page.tsx
@@ -1,13 +1,5 @@
-import { Sidebar } from "@/components/shared/sidebar";
 import { ServicesContainer } from "./containers/services-container";
 
 export default function ServicesPage() {
-  return (
-    <div className="flex min-h-screen bg-gray-50">
-      <Sidebar />
-      <main className="flex-1">
-        <ServicesContainer />
-      </main>
-    </div>
-  );
+  return <ServicesContainer />;
 }
