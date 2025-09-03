6e524e1a6cb69dd73f487e5dd72d022109552a7d
Hussein-alayan <alayen.hussein@gmail.com>
2025-09-03 15:46:27 +0300

    feat(seeder): add TestBookingRulesSeeder for creating booking rules with JSON format

---
+++ server/database/seeders/TestBookingRulesSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\BookingRule;
+
+class TestBookingRulesSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Clear existing booking rules for business 1
+        BookingRule::where('business_id', 1)->delete();
+
+        // Create booking rules for business 1 with JSON format
+        BookingRule::create([
+            'business_id' => 1,
+            'hours_of_operation' => [
+                'Monday' => ['start' => '09:00', 'end' => '17:00', 'available' => true],
+                'Tuesday' => ['start' => '09:00', 'end' => '17:00', 'available' => true],
+                'Wednesday' => ['start' => '09:00', 'end' => '17:00', 'available' => true],
+                'Thursday' => ['start' => '09:00', 'end' => '17:00', 'available' => true],
+                'Friday' => ['start' => '09:00', 'end' => '17:00', 'available' => true],
+                'Saturday' => ['start' => '10:00', 'end' => '16:00', 'available' => true],
+                'Sunday' => ['available' => false],
+            ],
+            'buffer_time_minutes' => 15,
+            'cancellation_policy' => '24 hours advance notice required',
+        ]);
+
+        $this->command->info('Created booking rules for business 1');
+    }
+}
