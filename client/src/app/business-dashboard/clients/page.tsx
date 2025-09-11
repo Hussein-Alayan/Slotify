ad4fa5887ec0dc45f9c6669c0383505c4a0bc48a
Hussein-alayan <alayen.hussein@gmail.com>
2025-09-11 16:08:38 +0300

    refactor: simplify ClientsPage component by removing Sidebar and unnecessary markup

--- client/src/app/business-dashboard/clients/page.tsx
+++ client/src/app/business-dashboard/clients/page.tsx
@@ -1,13 +1,5 @@
-import { Sidebar } from "@/shared/components/sidebar"
-import { ClientContainer } from "./containers/client-container"
+import { ClientContainer } from "./containers/client-container";
 
 export default function ClientsPage() {
-  return (
-    <div className="flex min-h-screen bg-gray-50">
-      <Sidebar />
-      <main className="flex-1">
-        <ClientContainer />
-      </main>
-    </div>
-  )
+  return <ClientContainer />;
 }
