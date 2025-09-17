75b0411849cadb9bd4a7c32676bffe9e57427073
Hussein-alayan <alayen.hussein@gmail.com>
2025-09-18 01:33:43 +0300

    feat: create CreateServiceRequest for service validation

---
+++ server/app/Http/Requests/CreateServiceRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateServiceRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'duration_minutes' => 'required|integer|min:1',
+            'price' => 'required|numeric|min:0',
+            'description' => 'nullable|string',
+            'status' => 'required|in:active,inactive',
+        ];
+    }
+}
