ad4c6476da620f1e7c6826d7611a5b43c1bf580d
Hussein-alayan <alayen.hussein@gmail.com>
2025-09-19 21:33:53 +0300

    feat: create CreateResourceRequest for resource validation with defined rules

---
+++ server/app/Http/Requests/CreateResourceRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateResourceRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'type' => 'required|in:staff,room,equipment',
+            'role' => 'nullable|string|max:255',
+            'special_skills' => 'nullable|string|max:255',
+            'availability' => 'nullable|array',
+            'business_id' => 'required|exists:businesses,id',
+        ];
+    }
+}
